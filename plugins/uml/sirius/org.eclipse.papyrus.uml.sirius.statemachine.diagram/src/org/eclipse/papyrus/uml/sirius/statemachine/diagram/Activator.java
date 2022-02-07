652836794e347ac72ee0f85970904bc370c12381
Ibtihel Khemir <ibtihel.khemir@cea.fr>
2022-02-07 14:01:49 +0100

    Bug 578616 - [Sirius][StateMachineDiagram]clean Papyrus-Sirius icons used by StateMachine Diagram and palette
    Change-Id: I1e27b9102ff184bd6697ebcb184f8fea79f9307a
    Signed-off-by: Ibtihel Khemir <ibtihel.khemir@cea.fr>

--- plugins/uml/sirius/org.eclipse.papyrus.uml.sirius.statemachine.diagram/src/org/eclipse/papyrus/uml/sirius/statemachine/diagram/Activator.java
+++ plugins/uml/sirius/org.eclipse.papyrus.uml.sirius.statemachine.diagram/src/org/eclipse/papyrus/uml/sirius/statemachine/diagram/Activator.java
@@ -19,81 +19,89 @@ import java.util.Set;
 import org.eclipse.sirius.business.api.componentization.ViewpointRegistry;
 import org.eclipse.sirius.viewpoint.description.Viewpoint;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.eclipse.uml2.uml.edit.UMLEditPlugin;
 import org.osgi.framework.BundleContext;
 
 /**
  * The activator class controls the plug-in life cycle.
  */
 public class Activator extends AbstractUIPlugin {
-    
-    /** The Constant PLUGIN_ID. */
-    public static final String PLUGIN_ID = "org.eclipse.papyrus.uml.sirius.statemachine.diagram";
 
-    /** The plugin. */
-    private static Activator plugin;
+	/** The Constant PLUGIN_ID. */
+	public static final String PLUGIN_ID = "org.eclipse.papyrus.uml.sirius.statemachine.diagram";
 
-    /** The viewpoints. */
-    private static Set<Viewpoint> viewpoints; 
+	/** The plugin. */
+	private static Activator plugin;
 
-    /**
-     * The constructor.
-     */
-    public Activator() {
+	/** The viewpoints. */
+	private static Set<Viewpoint> viewpoints;
+
+	/**
+	 * The constructor.
+	 */
+	public Activator() {
+		// to force the java dependency
+		// we need these plugins because the odesign references icons from this plugin
+		UMLEditPlugin.getPlugin();
 	}
 
-    /**
-     * Start.
-     *
-     * @param context the context
-     * @throws Exception the exception
-     * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
-     */
-    
-    /*
-     * (non-Javadoc)
-     * 
-     * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
-     */
-    @Override
+	/**
+	 * Start.
+	 *
+	 * @param context
+	 *            the context
+	 * @throws Exception
+	 *             the exception
+	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
+	 */
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
+	 */
+	@Override
 	public void start(BundleContext context) throws Exception {
-      super.start(context);
-	  plugin = this;
-	  viewpoints = new HashSet<Viewpoint>();
-	  viewpoints.addAll(ViewpointRegistry.getInstance().registerFromPlugin(PLUGIN_ID + "/description/papyrus_statemachine.odesign")); 
-    }
-
-    /**
-     * Stop.
-     *
-     * @param context the context
-     * @throws Exception the exception
-     * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
-     */
-    
-    /*
-     * (non-Javadoc)
-     * 
-     * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
-     */
-    @Override
-	public void stop(BundleContext context) throws Exception {
-	plugin = null;
-	if (viewpoints != null) {
-	    for (final Viewpoint viewpoint: viewpoints) {
-		ViewpointRegistry.getInstance().disposeFromPlugin(viewpoint);
-	    }
-	    viewpoints.clear();
-	    viewpoints = null; 
+		super.start(context);
+		plugin = this;
+		viewpoints = new HashSet<Viewpoint>();
+		viewpoints.addAll(ViewpointRegistry.getInstance().registerFromPlugin(PLUGIN_ID + "/description/papyrus_statemachine.odesign"));
 	}
-	super.stop(context);
-    }
 
-    /**
-     * Returns the shared instance.
-     *
-     * @return the shared instance
-     */
-    public static Activator getDefault() {
-	return plugin;
-    }
+	/**
+	 * Stop.
+	 *
+	 * @param context
+	 *            the context
+	 * @throws Exception
+	 *             the exception
+	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
+	 */
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
+	 */
+	@Override
+	public void stop(BundleContext context) throws Exception {
+		plugin = null;
+		if (viewpoints != null) {
+			for (final Viewpoint viewpoint : viewpoints) {
+				ViewpointRegistry.getInstance().disposeFromPlugin(viewpoint);
+			}
+			viewpoints.clear();
+			viewpoints = null;
+		}
+		super.stop(context);
+	}
+
+	/**
+	 * Returns the shared instance.
+	 *
+	 * @return the shared instance
+	 */
+	public static Activator getDefault() {
+		return plugin;
+	}
 }
