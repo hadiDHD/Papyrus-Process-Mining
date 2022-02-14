211af142254635b6323242f343413045f00f1732
Vincent Lorenzo <vincent.lorenzo@cea.fr>
2022-02-14 08:13:26 +0100

    Bug 578253: [Sirius][Tests] Some provided JUnit tests are not working  - fix Enumeration Semantic Drop
    Signed-off-by: Vincent Lorenzo <vincent.lorenzo@cea.fr>
    Change-Id: If1135529f28b5ce68a5ffad11dcf31a58eba1340

---
+++ tests/junit/plugins/uml/siriusdiag/org.eclipse.papyrus.uml.siriusdiag.clazz.tests/src/org/eclipse/papyrus/uml/siriusdiag/clazz/tests/drop/topNodes/AbstractTopNodeDropTest.java
@@ -0,0 +1,102 @@
+/*****************************************************************************
+ * Copyright (c) 2022 CEA LIST
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *  Vincent Lorenzo (CEA LIST) <vincent.lorenzo@cea.fr> - Initial API and implementation
+ *****************************************************************************/
+package org.eclipse.papyrus.uml.siriusdiag.clazz.tests.drop.topNodes;
+
+import org.eclipse.gmf.runtime.diagram.ui.editparts.DiagramEditPart;
+import org.eclipse.gmf.runtime.notation.Diagram;
+import org.eclipse.papyrus.sirusdiag.junit.utils.rules.SiriusDiagramEditorFixture;
+import org.eclipse.papyrus.uml.sirius.clazz.diagram.internal.constants.SemanticDropToolsIds;
+import org.eclipse.sirius.diagram.DDiagram;
+import org.eclipse.sirius.diagram.DDiagramElement;
+import org.eclipse.sirius.diagram.DNodeList;
+import org.eclipse.uml2.uml.Element;
+import org.eclipse.uml2.uml.Package;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Rule;
+
+/**
+ * Abstract Class for Drop Top Node tests
+ */
+public abstract class AbstractTopNodeDropTest {
+
+	/**
+	 * this fixture is used to access to Papyrus environment (editor/diagram/commandstack/editingdomain/...)
+	 */
+	@Rule
+	public final SiriusDiagramEditorFixture fixture = new SiriusDiagramEditorFixture();
+
+	/**
+	 * The root of the model
+	 */
+	protected Package root;
+
+	/**
+	 * Init the test field
+	 */
+	@Before
+	public void setUp() {
+		this.root = (Package) this.fixture.getModel();
+		Assert.assertNotNull(this.root);
+	}
+
+	/**
+	 * 
+	 * @param elementToBeDropped
+	 *            the element to drop
+	 * @param dropToolId
+	 *            the id of the drop tool to use
+	 * @param elementMappingType
+	 *            the expected mapping type of the created view
+	 */
+	protected void dropDNodeListElement(final Element elementToBeDropped, final String dropToolId, final String elementMappingType) {
+		final DiagramEditPart diagramEditpart = fixture.getActiveDiagram();
+		final Diagram diagram = diagramEditpart.getDiagramView();
+		Assert.assertEquals("The diagram must not yet have children.", 0, diagram.getChildren().size()); //$NON-NLS-1$
+
+		final DDiagram diagramRepresentation = (DDiagram) diagram.getElement();
+		Assert.assertEquals("The diagram representation must not yet have children.", 0, diagramRepresentation.getDiagramElements().size());//$NON-NLS-1$
+		fixture.applyContainerDropDescriptionTool(diagramRepresentation, dropToolId, diagramRepresentation, elementToBeDropped);
+		fixture.flushDisplayEvents();
+
+		Assert.assertEquals("The diagram must have one child after the Drop action.", 1, diagram.getChildren().size()); //$NON-NLS-1$
+		Assert.assertEquals("The diagram representation must have one child after the Drop action.", 1, diagramRepresentation.getDiagramElements().size());//$NON-NLS-1$
+		final DDiagramElement elementRepresentation = diagramRepresentation.getDiagramElements().get(0);
+		Assert.assertEquals("The mapping type of the created representation is not the expected one.", elementMappingType, elementRepresentation.getMapping().getName());//$NON-NLS-1$
+		Assert.assertTrue("The created sirus node must be a DNodeList", elementRepresentation instanceof DNodeList);//$NON-NLS-1$
+		Assert.assertEquals("Only one semantic element must be associated to the representation.", 1, ((DNodeList) elementRepresentation).getSemanticElements().size());//$NON-NLS-1$
+		Assert.assertTrue("The semantic element associated to the view must be the dropped element.", elementToBeDropped == ((DNodeList) elementRepresentation).getSemanticElements().get(0));//$NON-NLS-1$
+
+		// undo
+		fixture.getEditingDomain().getCommandStack().undo();
+		fixture.flushDisplayEvents();
+		Assert.assertEquals("The diagram must contain any children after undoig the Drop action", 0, diagram.getChildren().size()); //$NON-NLS-1$
+		Assert.assertEquals("The diagram representation must contain any children after undoig the Drop action", 0, diagramRepresentation.getDiagramElements().size()); //$NON-NLS-1$
+
+		// redo
+		fixture.getEditingDomain().getCommandStack().redo();
+		fixture.flushDisplayEvents();
+		Assert.assertEquals("The diagram must have one child after redoing of the Drop action.", 1, diagram.getChildren().size()); //$NON-NLS-1$
+		Assert.assertEquals("The diagram representation must have one child after redoing of the Drop action.", 1, diagramRepresentation.getDiagramElements().size());//$NON-NLS-1$
+	}
+
+	/**
+	 * Release resources
+	 */
+	@After
+	public void tearDown() {
+		this.root = null;
+	}
+}
--- tests/junit/plugins/uml/siriusdiag/org.eclipse.papyrus.uml.siriusdiag.clazz.tests/src/org/eclipse/papyrus/uml/siriusdiag/clazz/tests/drop/topNodes/Enumeration_TopNode_DropTest.java
+++ tests/junit/plugins/uml/siriusdiag/org.eclipse.papyrus.uml.siriusdiag.clazz.tests/src/org/eclipse/papyrus/uml/siriusdiag/clazz/tests/drop/topNodes/Enumeration_TopNode_DropTest.java
@@ -13,68 +13,29 @@
  *****************************************************************************/
 package org.eclipse.papyrus.uml.siriusdiag.clazz.tests.drop.topNodes;
 
-import org.eclipse.emf.ecore.EObject;
-import org.eclipse.gmf.runtime.diagram.ui.editparts.DiagramEditPart;
-import org.eclipse.gmf.runtime.notation.Diagram;
-import org.eclipse.gmf.runtime.notation.View;
-import org.eclipse.papyrus.junit.framework.classification.tests.AbstractPapyrusTest;
 import org.eclipse.papyrus.junit.utils.rules.ActiveDiagram;
 import org.eclipse.papyrus.junit.utils.rules.PluginResource;
-import org.eclipse.papyrus.sirusdiag.junit.utils.rules.SiriusDiagramEditorFixture;
-import org.eclipse.sirius.diagram.DDiagram;
-import org.eclipse.sirius.diagram.DNode;
-import org.eclipse.uml2.uml.Model;
+import org.eclipse.papyrus.uml.sirius.clazz.diagram.internal.constants.MappingTypes;
+import org.eclipse.papyrus.uml.sirius.clazz.diagram.internal.constants.SemanticDropToolsIds;
+import org.eclipse.uml2.uml.Enumeration;
 import org.eclipse.uml2.uml.NamedElement;
 import org.junit.Assert;
-import org.junit.Rule;
 import org.junit.Test;
 
 /**
- * Drop Class test
+ * Enumeration Class test
  */
 @PluginResource("resources/drop/topNode/TopNode_DropTest.di")
-@SuppressWarnings("nls")
-public class Enumeration_TopNode_DropTest extends AbstractPapyrusTest {
+public class Enumeration_TopNode_DropTest extends AbstractTopNodeDropTest {
 
-	private static final String CLASS_DIAGRAM_NAME = "TopNode_Drop_ClassDiagram";
+	private static final String CLASS_DIAGRAM_NAME = "TopNode_Drop_ClassDiagram"; //$NON-NLS-1$
 
-	@Rule
-	public final SiriusDiagramEditorFixture fixture = new SiriusDiagramEditorFixture(/* Collections.singletonList("aird") */);
-
-	@SuppressWarnings("restriction")
+	
 	@Test
 	@ActiveDiagram(CLASS_DIAGRAM_NAME)
-	public void Enumeration_DropTest() {
-
-		Assert.assertTrue(fixture.getModel() instanceof Model);
-		Model rootModel = (Model) fixture.getModel();
-		NamedElement elementToBeDropped = rootModel.getMember("EnumerationToDrop");
-		Assert.assertTrue("The element to be dropped is an instance of UML PrimitiveType", elementToBeDropped instanceof org.eclipse.uml2.uml.Enumeration);
-
-		DiagramEditPart diagramEditpart = fixture.getActiveDiagram();
-		Diagram diagram = diagramEditpart.getDiagramView();
-		Assert.assertEquals("The diagram must not yet have children", 0, diagram.getChildren().size());
-
-		DDiagram diagramRepresentation = (DDiagram) diagram.getElement();
-		fixture.applyContainerDropDescriptionTool(diagramRepresentation, "Class Enumeration from Model", diagramRepresentation, elementToBeDropped);
-		fixture.flushDisplayEvents();
-
-		Assert.assertEquals("The diagram must have one child after the Drop action", 1, diagram.getChildren().size());
-
-		// undo
-		fixture.getEditingDomain().getCommandStack().undo();
-		fixture.flushDisplayEvents();
-		Assert.assertEquals("The diagram does not contain any children after undoing the Drop action", 0, diagram.getChildren().size());
-
-		// redo
-		fixture.getEditingDomain().getCommandStack().redo();
-		fixture.flushDisplayEvents();
-		Assert.assertEquals("The diagram contains one child after redoing the Drop action", 1, diagram.getChildren().size());
-		Object element = diagram.getChildren().get(0);
-		Assert.assertTrue("The created element must be a View", element instanceof View);
-		EObject siriusNewRepresentation = ((View) element).getElement();
-		Assert.assertTrue("The created sirus node must be a DNode", siriusNewRepresentation instanceof DNode);
-		EObject semanticElement = ((DNode) siriusNewRepresentation).getSemanticElements().iterator().next();
-		Assert.assertTrue("The created element must be a UML Enumeration", semanticElement instanceof org.eclipse.uml2.uml.Enumeration);
+	public void enumeration_DropTest() {
+		final NamedElement elementToBeDropped = this.root.getMember("EnumerationToDrop"); //$NON-NLS-1$
+		Assert.assertTrue("The element to be dropped must be an instance of Enumeration", elementToBeDropped instanceof Enumeration);//$NON-NLS-1$
+		dropDNodeListElement(elementToBeDropped, SemanticDropToolsIds.DROP_ENUMERATION_TOOL, MappingTypes.ENUMERATION_NODE_TYPE);
 	}
 }
